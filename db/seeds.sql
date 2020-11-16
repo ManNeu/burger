f7454ace2601fa63ace95d70cbde47bd9ec66384
Manish Neupane <mrnish.are@gmail.com>
2020-11-17 01:07:44 +1100

    adding all files for burger app

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chicken burger', 1),
+    ('goat burger', 0),
+    ('veggie burger', 0);
+-- SELECT *
+-- FROM burgers;
+
+
+--  In this file, write insert queries to populate the burgers table with at least three entries.
